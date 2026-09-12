e9cd2b13063d26668b7ee6fe6cb890af23fca809
Atharv Prabhu <atharvprabhu401@gmail.com>
2026-09-12 10:59:07 +0530

    deploy 1 no BE

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig({
   plugins: [
     tanstackStart({
       server: { entry: "server" },
+      nitro: {
+        preset: "vercel",
+      },
     }),
     react(),
     tailwindcss(),
